d6c6a5c680a71a87c6a2d2675736c9a8c989c1fa
lj360560179 <360560179@qq.com>
2018-06-19 15:46:21 +0800

    nettystudy

---
+++ src/main/java/com/lj/nettyaction/EchoClient.java
@@ -0,0 +1,8 @@
+package com.lj.nettyaction;
+
+/**
+ * @Author lj
+ * @Date 2018/6/19 15:46
+ */
+public class EchoClient {
+}
---
+++ src/main/java/com/lj/nettyaction/Server.java
@@ -0,0 +1,40 @@
+package com.lj.nettyaction;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @Author lj
+ * @Date 2018/6/19 15:32
+ */
+public class Server {
+
+    public static void main(String[] args) {
+
+    }
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(9999))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerHandle());
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
