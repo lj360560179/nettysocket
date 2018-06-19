348b978cc0157232034362fee846c46e206b7ef0
lj360560179 <360560179@qq.com>
2018-06-19 11:46:26 +0800

    netty

--- src/main/java/com/lj/nettysocket/server/IMServer.java
+++ src/main/java/com/lj/nettysocket/server/IMServer.java
@@ -14,8 +14,6 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
